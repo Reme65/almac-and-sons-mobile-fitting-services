cfc0e5e342eab0422a286733ba45d5de07a84dae
Reme65 <amreme168@gmail.com>
2026-09-26 20:17:05 +0100

    Integrate services page into Django

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from assistance import views as assistance_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", core_views.home, name="home"),
+    path("services/", core_views.services, name="services"),
     path(
         "assistance/",
         assistance_views.request_assistance,
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ServicesViewTests(TestCase):
+
+    def test_services_page_renders_services_template(self):
+        response = self.client.get(reverse("services"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "services.html")
+        
